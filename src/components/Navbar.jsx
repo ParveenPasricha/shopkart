3df86035bee5a6a49709d086d5480a3bcba5d1a2
ParveenPasricha <184731702+ParveenPasricha@users.noreply.github.com>
2024-11-05 05:02:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Shopping from './Pages/Shopping'
+import Cart from './Pages/Cart'
+import Login from './Login';
+
+const Navbar = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}>Home</Route>
+      <Route path="/shopping" element={<Shopping/>}>Shopping</Route>
+      <Route path="/cart" element={<Cart/>}>Cart</Route>
+      <Route path="/login" element={<Login/>}>Login/SignUP</Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Navbar
